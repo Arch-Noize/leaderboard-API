e65ea1eb7e113b8632aa9a0543b5792b983e742c
Arch-Noize <santiagonoize@gmail.com>
2023-08-01 11:02:06 -0400

    Fix styling

--- src/index.js
+++ src/index.js
@@ -1,7 +1,43 @@
 // import { score , storeScore } from './modules/storage';
 import './index.css';
 
+const leaderboard = [
+  {
+    user: 'Jame',
+    score: '1450',
+  },
+  {
+    user: 'Oreo',
+    score: '1234',
+  },
+  {
+    user: 'Z',
+    score: '0000',
+  },
+  {
+    user: 'CJ',
+    score: '3190',
+  },
+  {
+    user: 'San',
+    score: '999',
+  },
+];
+
+const scores = document.querySelector('.recent-scores');
 const addBtn = document.querySelector('.add');
+
+const displayScore = () => {
+  leaderboard.forEach((item) => {
+    scores.innerHTML += `
+    <li class="score">
+      ${item.user}: ${item.score}
+    </li>
+    `
+  })
+}
+
+window.addEventListener('DOMContentLoaded', displayScore);
 
 addBtn.addEventListener('click', (e) => {
   e.preventDefault();
