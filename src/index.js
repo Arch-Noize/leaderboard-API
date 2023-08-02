fd8587f82e45bf332c210353a24fe0ecbac77d75
Arch-Noize <santiagonoize@gmail.com>
2023-08-02 10:14:14 -0400

    Fix display error

--- src/index.js
+++ src/index.js
@@ -6,12 +6,16 @@ const addBtn = document.querySelector('.add');
 const refresh = document.querySelector('.refresh');
 const form = document.querySelector('#new-score');
 
-refresh.addEventListener('click', async () => {
-  scores.innerHTML = '';
+const displayScores = async () => {
   const leaderboard = await getScores();
   leaderboard.forEach((item) => {
     scores.innerHTML += `<li class="score">${item.user}: ${item.score}</li>`;
   });
+}
+
+refresh.addEventListener('click', () => {
+  scores.innerHTML = '';
+  displayScores();
 });
 
 addBtn.addEventListener('click', async (e) => {
@@ -25,3 +29,5 @@ addBtn.addEventListener('click', async (e) => {
     form.reset();
   }
 });
+
+window.addEventListener('DOMContentLoaded', displayScores);
